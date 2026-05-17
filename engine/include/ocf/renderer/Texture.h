9198bb4801f73526c8446bbf798659ea799076cb
Tsuyoshi KATAYAMA <katayama933@gmail.com>
2026-05-17 23:20:19 +0900

    Texture クラスと関連機能の追加
    `Texture` クラスを追加し、`Builder` クラスやテクスチャの
    ハンドル取得、画像設定、終了処理のメソッドを実装。
    `Texture.cpp` に `BuilderDetails` 構造体を追加し、関連する
    メソッドの定義を追加（実装は未完成）。

--- engine/include/ocf/renderer/Texture.h
+++ engine/include/ocf/renderer/Texture.h
@@ -1,9 +1,47 @@
 #pragma once
 
+#include "ocf/rhi/Handle.h"
+#include "ocf/rhi/RHIEnums.h"
+
 namespace ocf {
 
-class Texture {
+class Engine;
 
+class Texture {
+    struct BuilderDetails;
+
+public:
+    static constexpr size_t BASE_LEVEL = 0;
+
+    using TextureHandle = rhi::TextureHandle;
+
+    class Builder {
+    public:
+        Builder();
+        ~Builder();
+        BuilderDetails* operator->() noexcept { return m_impl; }
+        const BuilderDetails* operator->() const noexcept { return m_impl; }
+
+        Texture* build(Engine& engine);
+
+    private:
+        BuilderDetails* m_impl = nullptr;
+    };
+
+    Texture(Engine& engine, const Builder& builder);
+
+    void terminate(Engine& engine);
+
+    TextureHandle getHandle() const noexcept { return m_handle; }
+
+    void setImage(uint32_t width, uint32_t height);
+
+private:
+    TextureHandle m_handle;
+    uint32_t m_width = 1;
+    uint32_t m_height = 1;
+    uint8_t m_levelCount = 1;
+    uint32_t m_depth = 1;
 };
 
 } // namespace ocf
--- engine/src/renderer/Texture.cpp
+++ engine/src/renderer/Texture.cpp
@@ -2,6 +2,22 @@
 
 namespace  ocf {
 
+struct Texture::BuilderDetails {
+    uint32_t width;
+    uint32_t height;
+};
 
+
+Texture::Texture(Engine& engine, const Builder& builder)
+{
+}
+
+void Texture::terminate(Engine& engine)
+{
+}
+
+void Texture::setImage(uint32_t width, uint32_t height)
+{
+}
 
 } // namespace ocf
